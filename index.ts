8c755c63e589094b75f3a63c8266f489424343f3
AlexDev404 <AlexDev404@users.noreply.github.com>
2023-07-31 19:51:54 -0600

    Add .gitignore

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 // main.ts
 import data from "./symbolsData";
+import { SvelteComponent } from "svelte";
 
 const nameToSymbolLookup: Record<string, string> = {};
 
@@ -12,3 +13,16 @@ for (const item of data) {
 export function getSFSymbolByName(name: string): string | undefined {
   return nameToSymbolLookup[name];
 }
+
+// Define the props interface for the SFIcon component
+interface SFIconProps {
+  icon: string;
+  class?: string;
+  size?: string;
+  weight?: string;
+  color?: string;
+  rounded?: boolean;
+}
+
+// Define the SFIcon component
+export default class SFIcon extends SvelteComponent<SFIconProps> {}
